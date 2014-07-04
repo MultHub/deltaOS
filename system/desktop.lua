c013c54b5098df01edaa947e3c29d24db8be8ca0
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-07-04 12:58:36 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -14,28 +14,7 @@ fullBuildName = "DeltaOS Unstable(build "..build..")"
 
 os.loadAPI("/apis/users")
 
-http.request("http://deltaos.netii.com/ip.php")
 
-local requesting = true
-
-while requesting do
-  local event, url, sourceText = os.pullEvent()
-  
-  if event == "http_success" then
-    local respondedText = sourceText.readAll()
-    
-    sourceText.close()
-    if respondedText == "208.111.31.84" then
-     error("IP BLOCKED", 0)
-    end
-    
-    requesting = false
-  elseif event == "http_failure" then
-    --print("Server didn't respond.")
-    
-    requesting = false
-  end
-end
 
 local function clear()
 term.current().setBackgroundColor(colors.lightBlue)
